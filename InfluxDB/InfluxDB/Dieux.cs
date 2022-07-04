bfe059821c09c54de28efc10f32f232bc2fc74fb
Yoan Laurain <80961060+Yoan-laurain@users.noreply.github.com>
2022-07-04 22:36:15 +0200

    Implementation of connexion read and write methods

---
+++ InfluxDB/InfluxDB/Dieux.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfluxDB
+{
+    class Dieux
+    {
+        public string nom;
+        public string time;
+
+        public Dieux(string nom, string time)
+        {
+            this.nom = nom;
+            this.time = time;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Time { get => time; set => time = value; }
+    }
+}
---
+++ InfluxDB/InfluxDB/InfluxDBService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using InfluxDB.Client;
+using Microsoft.Extensions.Configuration;
+
+namespace InfluxDB
+{
+    namespace app.Services
+    {
+        public class InfluxDBService
+        {
+            private readonly string _token = "tJbAwoN6Jd26K-w7TRSAkOo6FWFv4GVpiYky2cGLqYAjKnBZeOG9s1JEeEA-rR8MGaFgrjrrUtRlobGsTkxkxA==";
+
+            /*public InfluxDBService(IConfiguration configuration)
+            {
+                _token = configuration.GetConnectionString("InfluxDB:Token");
+            }*/
+
+            public void Write(Action<WriteApi> action)
+            {
+                var client = InfluxDBClientFactory.Create("http://localhost:8086", _token);
+                var write = client.GetWriteApi();
+                action(write);
+                Console.WriteLine("ICI");
+            }
+
+            public async Task<T> QueryAsync<T>(Func<QueryApi, Task<T>> action)
+            {
+                var client = InfluxDBClientFactory.Create("http://localhost:8086", _token);
+                var query = client.GetQueryApi();
+                return await action(query);
+            }
+        }
+    }
+}
